e726e2636aa51d5b0b6dfd6d168bf828f5a3a26c
Zaher ABBAS <z.abbas83@yahoo.com>
2025-10-14 15:39:05 +0200

    refactor profile layout for improved user experience and styling

--- src/Vue/profile.php
+++ src/Vue/profile.php
@@ -13,10 +13,10 @@
                     src="https://img.favpng.com/23/3/2/computer-icons-silhouette-user-profile-png-favpng-NvqKckg9G8mZk9Qi1zqWnn4fA.jpg"
                     class="card-img" alt="...">
             </div>
-            <div class="card-body col-8">
-                <h3 class="card-title">&#128100; <?= htmlspecialchars($user["firstname"]) . " " . htmlspecialchars($user['lastname'])?></h3>
-                <p class="card-text fs-3">📧 <?= htmlspecialchars($user["email"])?> </p>
-                <a href="index.php" class="btn btn-primary">Back</a>
+            <div class="card-body col-8 d-flex flex-column align-items-center justify-content-around">
+                <h3 class="card-title align-self-start">&#128100; <?= htmlspecialchars($user["firstname"]) . " " . htmlspecialchars($user['lastname'])?></h3>
+                <p class="card-text fs-3 align-self-start">📧 <?= htmlspecialchars($user["email"])?> </p>
+                <a href="index.php" class="btn btn-outline-danger">Back</a>
             </div>
         </div>
     </div>
