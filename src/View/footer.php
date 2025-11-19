ab9c5c68b1e0a547900541a6c542f5420b157403
Zaher ABBAS <z.abbas83@yahoo.com>
2025-11-19 10:08:49 +0100

    update header and footer: refine button padding and spacing for better alignment; replace "MyRecipe" with "CookShare" in footer for branding consistency

--- src/View/footer.php
+++ src/View/footer.php
@@ -1,5 +1,5 @@
 <footer class=" mt-auto d-flex justify-content-center align-items-center p-4">
     <div class="text-white text-center small">
-        &copy; <?= date('Y') ?> MyRecipe — <span aria-label="amour">❤️</span> par Zaher ABBAS
+        &copy; <?= date('Y') ?> CookShare — <span aria-label="amour">❤️</span> By Zaher ABBAS
     </div>
 </footer>
